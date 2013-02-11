332ad9bc3881e8441a455544b78756cfd4ef327a
Jonathan <jonathan@jonathan-SVE11125CLB.(none)>
2013-02-11 15:46:44 -0430

    Revirtiendo

--- lib/form/EncuestaForm.class.php
+++ lib/form/EncuestaForm.class.php
@@ -15,7 +15,6 @@ class EncuestaForm extends BaseEncuestaForm
       $this->widgetSchema['tipo_encuesta']= new sfWidgetFormChoice(array('choices' => $choices));
       
       $this->widgetSchema['descripcion_encuesta'] = new sfWidgetFormTextarea(array(), array('cols' => 40, 'rows' => 8));
-      /*
           $this->widgetSchema['fecha_elaboracion']=  new sfWidgetFormJQueryDate(array(
       'image'=>'/images/calendar.png', 'culture' => 'es', 
       'date_widget' => new sfWidgetFormDate(array('format' => '%day% / %month% / %year% ')),
@@ -23,12 +22,13 @@ class EncuestaForm extends BaseEncuestaForm
         ));
       $this->validatorSchema['fecha_elaboracion'] = new sfValidatorPass();
       
+      /*
       $this->widgetSchema['hora'] = new sfWidgetFormTime(array(
       'with_seconds' => true,'can_be_empty'=> false, 
       'format'       => '%hour% : %minute%',
       'default' => date('HH:mm')
     ));
 */
- $this->validatorSchema['hora'] = new sfValidatorPass();  
+ $this->validatorSchema['_csrf_token'] = new sfValidatorPass();  
   }
 }
