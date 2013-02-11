4f0a75d4c602735fc0e05a7b6b2c490aa2d0e9de
mwveliz <mwveliz@mw>
2013-02-11 14:57:59 -0430

    solo agregarvarios de item

--- apps/frontend/modules/item/templates/agregarvariosSuccess.php
+++ apps/frontend/modules/item/templates/agregarvariosSuccess.php
@@ -2,7 +2,7 @@
 <?php use_javascripts_for_form($form) ?>
 <?php
 //actualizo el id_item por si hay alguna falla
-$numeracion=0;
+$numeracion=1;
 $id_encuesta=$sf_params->get('id_encuesta');
 $I=  ItemQuery::create()->filterByIdEncuesta($id_encuesta)->orderById('desc')->findOne();
 if (count($I)>0) $numeracion=$I->getNumeracion()+1;
@@ -74,7 +74,7 @@ if (count($I)>0) $numeracion=$I->getNumeracion()+1;
      <tr class="tr_ocultable" tipo="C" style="display:none" id="1">
         <td id="1" >
             <input type="radio" name="opcion[]" id="C" checked>
-                <input type="text" id="1"/>
+                <input type="text" id="opcion" class="C"/>
                 </input>
         </td>
         <td id="mas">
@@ -87,7 +87,7 @@ if (count($I)>0) $numeracion=$I->getNumeracion()+1;
     <tr class="tr_ocultable" tipo="D" style="display:none" id="1">
         <td id="1" >
             <input type="checkbox" name="opcion[]" id="D" checked="checked">
-                    <input type="text" id="1"/>
+                    <input type="text" id="opcion" class="D"/>
                 </input>
         </td>
         <td id="mas">
@@ -97,7 +97,7 @@ if (count($I)>0) $numeracion=$I->getNumeracion()+1;
     <tr class="tr_ocultable" tipo="E" style="display:none" id="1">
         <td id="1" >
             <input type="checkbox" name="opcion[]" id="E" checked="checked">
-                <input type="text" id="1"/>_____________
+                <input type="text" id="opcion" class="E"/>_____________
                 </input>
         </td>
         <td id="mas">
@@ -108,7 +108,7 @@ if (count($I)>0) $numeracion=$I->getNumeracion()+1;
     <tr class="tr_ocultable" tipo="F" style="display:none" id="1">
         <td id="1" >
             <input type="checkbox" name="opcion[]" id="F" checked="checked">
-                <input type="text" id="1"/>
+                <input type="text" id="opcion" class="F"/>
                 </input>
         </td>
         <td id="mas">
@@ -129,7 +129,7 @@ if (count($I)>0) $numeracion=$I->getNumeracion()+1;
         
         <td id="1" colspan="2" >
             <input type="checkbox" name="opcion[]" id="G" checked="checked" id="1">
-               <input type="text" id="1"/>
+               <input type="text" id="opcion" class="G"/>
                </input>
         </td>
         <td id="mas">
@@ -137,7 +137,7 @@ if (count($I)>0) $numeracion=$I->getNumeracion()+1;
         </td>
         
     </tr>      
-    <tr class="tr_ocultable" tipo="H" style="display:none">
+    <tr class="tr_ocultable" tipo="H" style="display:none" id="1">
          <td id="max_sub">
             Numero de sub items: <select id="max_sub">
                             <option>1</option>
@@ -154,7 +154,7 @@ if (count($I)>0) $numeracion=$I->getNumeracion()+1;
         </td>
         <td id="1" colspan="2" >
             <input type="checkbox" name="opcion[]" id="H" checked="checked" id="1">
-               <input type="text" id="1"/>
+               <input type="text" id="opcion" class="H"/>
                </input>
         </td>
         <td id="mas">
@@ -184,9 +184,11 @@ $(document).ready(function(){
 
   $(document).on("click","td#mas", function(){
         $(this).parent().clone().appendTo('table.tablas');
+        $('tr:last').attr("param","clonado" );
+        $('input#opcion:last').val("");
         /*var indice=$('tr.tr_ocultable:last').index()-9;
         indice++;*/
-        $(this).remove();
+        $(this).toggle();
         /*$('tr.tr_ocultable:last').attr('id',indice);
         */
         $('td#max_escala').not('td#max_escala:first').remove();
@@ -203,6 +205,7 @@ $(document).ready(function(){
      
      
      
+     
 });
 
 
@@ -214,11 +217,16 @@ $('input#siguiente_submit').click(function (e){
     var opcion=new Array;
     var i=0;
     var valor='';
-    
     var tipo_item=$('select#tipo_item').val();
+    alert(tipo_item);
+    
     var max_escala=$('select#max_escala').val();
     var max_sub=$('select#max_sub').val();
     var id_encuesta=getidencuesta(); 
+    var max=0;
+    if(tipo_item=='G') max=max_escala;
+    if(tipo_item=='H') max=max_sub;
+    
     
     $('input[class="'+tipo_item+'"]').each(function(){
        valor=$(this).val() ;
@@ -231,14 +239,18 @@ $('input#siguiente_submit').click(function (e){
     $.ajax({ type: "POST",
 	    	url: "<?php echo url_for('item/createvarios'); ?>",
                 data: "numeracion=" + numeracion + "&id_encuesta="+id_encuesta
-                +"&texto=" + texto+"&tipo_item="+ tipo_item+"&opcion="+JSON.stringify(opcion),
+                +"&texto=" + texto+"&tipo_item="+ tipo_item+"&max="+max+"&opcion="+JSON.stringify(opcion),
 
 	    	success: function(message){
+                    $('tr[param="clonado"]').remove();
+                    $('td#mas').css('display','inline');
+                    
+                    $('input#opcion').val("");
                     numeracion++;
                   $('input#numeracion').val("");
                   $('input#numeracion').val(numeracion);
                   $('input#texto').val("");
-                  $('select#tipo_item').val('A');
+                  $('select#tipo_item').val('A').trigger('change');
                   $('select#tipo_item').trigger('click');
                   
                   
