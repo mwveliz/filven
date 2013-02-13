f05c5e4fa111956fee0a305e97b0c847d9cf8271
mwveliz <mwveliz@mw>
2013-02-12 19:35:29 -0430

    respuesta_encuesta

--- apps/frontend/modules/respuesta_encuesta/actions/actions.class.php
+++ apps/frontend/modules/respuesta_encuesta/actions/actions.class.php
@@ -36,11 +36,130 @@ class respuesta_encuestaActions extends sfActions
   {
     $this->forward404Unless($request->isMethod(sfRequest::POST));
 
-    $this->form = new RespuestaEncuestaForm();
+    //debo chequear si esoty creado o editando la encuesta antes de emepzar <<-- ṔENDIENTE
+    //LO MISMO QUE EN AGREGARITEM
+    $REncuesta = new RespuestaEncuesta();
 
-    $this->processForm($request, $this->form);
+     $id_encuesta=  $_POST['id_encuesta']; //fk a encuesta
+     $numero_encuesta=  $_POST['numero_encuesta']; //numeracion de la hoja
+     $nombre=  $_POST['nombre']; 
+     $apellido=  $_POST['apellido']; 
+     $fecha=  $_POST['fecha'].' '.$_POST['hora']; 
+     $observacion=  $_POST['observacion']; 
+     $telefono=  $_POST['telefono']; 
+     $email=  $_POST['email']; 
+ 
+     // die(var_dump($_POST));  
+     $REncuesta->setIdEncuesta($id_encuesta);
+     $REncuesta->setNumeroEncuesta($numero_encuesta);
+     $REncuesta->setNombre($nombre);
+     $REncuesta->setApellido($apellido);
+     $REncuesta->setFecha($fecha);
+     $REncuesta->setTelefono($telefono);
+     $REncuesta->setEmail($email);
+     $REncuesta->setObservacion($observacion);
+     $REncuesta->save();
+     
+     $id_respuesta_encuesta=$REncuesta->getid();
+     $Items= ItemQuery::create()->filterByIdEncuesta($id_encuesta)->orderByNumeracion('asc')->find();
+     
+     foreach ($Items as $Item){
+        $id_item=$Item->getId();
+        $tipo_item=$Item->getTipoItem();
 
+   
+        switch($tipo_item){
+            case "A": //caso entrada por completacion
+                $RItem = new RespuestaItem();
+                $RItem->setIdRespuestaEncuesta($id_respuesta_encuesta);
+                $RItem->setIdItem($id_item);
+                $RItem->setTipoItem($tipo_item);
+                $RItem->setValor($_POST[$id_item]);
+                $RItem->save();
+            break;
+            case "B": //caso seleccion simple radiobox
+                $RItem = new RespuestaItem();
+                $RItem->setIdRespuestaEncuesta($id_respuesta_encuesta);
+                $RItem->setIdItem($id_item);
+                $RItem->setTipoItem($tipo_item);
+                $RItem->setIdOpcion(intval($_POST[$id_item][0]));
+                $RItem->save();
+            break;
+            case "C": //caso radiobox otro/especifique
+                $RItem = new RespuestaItem();
+                $RItem->setIdRespuestaEncuesta($id_respuesta_encuesta);
+                $RItem->setIdItem($id_item);
+                $RItem->setTipoItem($tipo_item);
+                $RItem->setIdOpcion(intval($_POST[$id_item][0]));
+                if (isset($_POST[$id_item.'otro'])){
+                  $RItem->setValor($_POST[$id_item.'otro']);  //si agrego alguna opcion extra
+                } 
+                $RItem->save();
+            break;
+            case "D": //caso seleccion multiple cerrada
+                foreach($_POST[$id_item] as $key=>$value){
+                $RItem = new RespuestaItem();
+                $RItem->setIdRespuestaEncuesta($id_respuesta_encuesta);
+                $RItem->setIdItem($id_item);
+                $RItem->setTipoItem($tipo_item);
+                $RItem->setIdOpcion($value);
+                $RItem->save();    
+                }
+            break;
+            case "E": //caso completacion multiple 
+                foreach($_POST[$id_item] as $key=>$value){
+                $RItem = new RespuestaItem();
+                $RItem->setIdRespuestaEncuesta($id_respuesta_encuesta);
+                $RItem->setIdItem($id_item);
+                $RItem->setTipoItem($tipo_item);
+                $RItem->setValor($value);
+                $RItem->save();    
+                }
+            break;
+           case "F": //caso Checkbox otro/especifique
+                foreach($_POST[$id_item] as $key=>$value){
+                $RItem = new RespuestaItem();
+                $RItem->setIdRespuestaEncuesta($id_respuesta_encuesta);
+                $RItem->setIdItem($id_item);
+                $RItem->setTipoItem($tipo_item);
+                $RItem->setIdOpcion($value);
+                if (isset($_POST[$id_item.'otro'])){
+                  $RItem->setValor($_POST[$id_item.'otro']);  //si agrego alguna opcion extra
+                } 
+                $RItem->save();
+                }
+            break;
+          case "G": //caso opcion multiple
+             foreach($_POST[$id_item] as $opcion=>$valor){
+                $RItem = new RespuestaItem();
+                $RItem->setIdRespuestaEncuesta($id_respuesta_encuesta);
+                $RItem->setIdItem($id_item);
+                $RItem->setTipoItem($tipo_item);
+                $RItem->setIdOpcion($opcion);
+                $RItem->setValor($valor);
+                $RItem->save();
+             }
+          break;
+          case "H": //caso opcion multiple
+             foreach($_POST[$id_item] as $opcion => $valores){ //itero sobre las opciones
+              foreach($valores as $valor){ //obtengo los multiples valores de esas opciones y asigno cada uno
+                $RItem = new RespuestaItem();
+                $RItem->setIdRespuestaEncuesta($id_respuesta_encuesta);
+                $RItem->setIdItem($id_item);
+                $RItem->setTipoItem($tipo_item);
+                $RItem->setIdOpcion($opcion);
+                $RItem->setValor($valor);
+                $RItem->save();
+              }
+             }
+          break;
+        }
+     }//fin foreach items
+     
+     
     $this->setTemplate('new');
+    $this->redirect('respuesta_encuesta/new?id='.$id_encuesta);
+    
   }
 
   public function executeEdit(sfWebRequest $request)
--- apps/frontend/modules/respuesta_encuesta/templates/_form.php
+++ apps/frontend/modules/respuesta_encuesta/templates/_form.php
@@ -1,24 +1,135 @@
-<?php use_stylesheets_for_form($form) ?>
-<?php use_javascripts_for_form($form) ?>
+<? $nueva=true;?>
+<form action="<?php echo url_for('respuesta_encuesta/'.($nueva ? 'create' : 'update').(!$nueva ? '?id='.$id_encuesta: '')) ?>" method="post">
+<input type="hidden" value="<?echo $sf_params->get('id_encuesta')?>" name="id_encuesta">
 
-<form action="<?php echo url_for('respuesta_encuesta/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-<?php if (!$form->getObject()->isNew()): ?>
-<input type="hidden" name="sf_method" value="put" />
-<?php endif; ?>
-  <table>
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          &nbsp;<a href="<?php echo url_for('respuesta_encuesta/index') ?>">Back to list</a>
-          <?php if (!$form->getObject()->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'respuesta_encuesta/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-          <?php endif; ?>
-          <input type="submit" value="Save" />
+<table>
+    <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+   <tr>
+        <td colspan="1"> Numero de Encuesta:</td><td><input type="text" size="5" name="numero_encuesta"/></td>
+   </tr>     
+   <tr>
+       <td></td>
+        <td> Nombre:<input type="text" name="nombre"/></td>
+        <td colspan="1"> Apellido:<input type="text" name="apellido"/>
+        </td>
+         <td colspan="1"> Teléfono:<input type="text" name="telefono"/></td>
+        
+  </tr>      
+  <tr>
+      <td></td>
+        
+      <td colspan="1"> Fecha:<input type="date" name="fecha"/></td>
+        <td colspan="1"> Hora:<input type="time" name="hora"/> </td>
+        <td colspan="1"> email:<input type="text" name="email"/></td>
+</tr>
+   <tr>
+       
+<br>
+<br><br>
+        <td> Observación:</td><td><input type="textarea" name="observacion"/></td>
+   </tr>     
+
+
+
+<?php foreach ($Items as $Item): ?>
+<tr>
+  <td>
+      <?php 
+      $OpcionRespuestas = OpcionRespuestaQuery::create()->filterByIdItem($Item->getId())->find();
+      $cantidad_opciones=count($OpcionRespuestas);
+      $tipo_item=$Item->getTipoItem();
+      $id_item=$Item->getId();
+      echo $Item->getNumeracion() .'. ' . $Item->getTexto().'</td><td>' ;
+      if ($tipo_item=='A'){
+          echo "   <input type='text' name='". $id_item. "' id='". $id_item." '/>";
+      }
+      
+      if ($tipo_item=='G') {
+          $max_escala=$Item->getMaximo();
+          for ($i=1; $i<=$max_escala; $i++) {
+                echo $i."</td><td>";
+                }
+                echo "NS/NC";
+                //agrego un input oculto con el id_item
+                echo '<input type="hidden" value="" name="id_encuesta">';
+      }
+    
+      if ($tipo_item=='H') {
+          $max_sub=$Item->getMaximo();
+          for ($i=1; $i<=$max_sub; $i++) {
+                echo $i."</td><td>";
+                }
+      }
+    $i=0;
+      ?>
+   </td></tr>
+    <tr><td>
+    <?php foreach ($OpcionRespuestas as $OpcionRespuesta): ?>
+    <?php $i++;
+    $id_opcion=$OpcionRespuesta->getId();
+    switch($tipo_item) {
+       case "B"://seleccion simple
+           echo "<input type='radio' name='".$id_item."[]'  value='".$id_opcion."'/> ". $OpcionRespuesta->getOpcion();
+        break;
+       case "C"://seleccion con completacion
+           echo "<input type='radio' name='".$id_item."[]'  value='".$id_opcion."'/> ". $OpcionRespuesta->getOpcion();
+           if ($i==$cantidad_opciones){
+               echo "<input type='text' name='".$id_item."otro'  value=''/> ";
+           }
+        break;
+        case "D"://seleccion multilpe
+           echo "<input type='checkbox' name='".$id_item."[]' value='".$id_opcion."'/> ". $OpcionRespuesta->getOpcion();
+        break;
+
+        case "E"://completacion multiple
+               echo $OpcionRespuesta->getOpcion() .": <input type='text' name='".$id_item."[]'  id='".$id_item."'/> ";
+           
+        break;
+       case "F"://seleccion multiple con completacion
+           echo "<input type='checkbox' name='".$id_item."[]'  value='".$id_opcion."'/> ". $OpcionRespuesta->getOpcion();
+           if ($i==$cantidad_opciones){
+               echo "<input type='text' name='".$id_item."otro'  value=''/> ";
+           }
+        break;
+        case "G"://seleccion multiple con valores
+            echo $OpcionRespuesta->getOpcion()."</td><td>";
+              for ($i=1; $i<=$max_escala; $i++) {
+                echo "<input type='radio' name='".$id_item."[".$id_opcion."]'  value='". $i."'/> </td><td>";
+                }
+                echo "<input type='radio' name='".$id_item."[".$id_opcion."]'  value='". "'NS/NC'/></td></tr><tr><td> ";
+        break;
+        case "H"://seleccion multiple con valores
+            echo $OpcionRespuesta->getOpcion();
+            for ($i=0; $i<$max_sub; $i++) {
+                 echo "</td><td><input type='text'name='".$id_item."[".$id_opcion."][".$i."]' /></td><td>";
+                }
+                echo "</td></tr><tr><td> ";
+        break;  
+    }
+            ?>
+        <?php endforeach;?>
+    </td></tr>
+<?php endforeach;?>
+    
+  </tbody>   
+<tfoot>
+    
+        <td>
+            <input id="guardar_submit" type="submit" value="Guardar" />
+        </td>
+        
+        
+        <td>
+            <input id="cancelar_submit" type="submit" value="Cancelar" />
         </td>
       </tr>
     </tfoot>
-    <tbody>
-      <?php echo $form ?>
-    </tbody>
   </table>
 </form>
